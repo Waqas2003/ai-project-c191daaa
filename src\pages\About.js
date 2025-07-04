5b5c9d5958cb147bdb67a393cae4d3b3a2ccf9af
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 21:26:17 +0500

    Add src\pages\About.js

---
+++ src\pages\About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutComponent() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We have a team of experienced software developers with a proven track record of delivering high-quality solutions on time and within budget.</p>
+    </div>
+  );
+}
+
+export default AboutComponent;
