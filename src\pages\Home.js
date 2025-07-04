446e2d0e7ef751defbacbe496492a0f8b27b52c6
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 21:26:21 +0500

    Add src\pages\Home.js

---
+++ src\pages\Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomeComponent() {
+  return (
+    <div>
+      <h1>Welcome to Software Developer Agent</h1>
+      <p>We are a team of experienced software developers dedicated to providing top-notch solutions for your business.</p>
+    </div>
+  );
+}
+
+export default HomeComponent;
