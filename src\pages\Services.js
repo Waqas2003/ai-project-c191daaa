2b4f1503c4d955b40a725e98bcc0b194a3b8b1bc
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 21:26:23 +0500

    Add src\pages\Services.js

---
+++ src\pages\Services.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ServicesComponent() {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <ul>
+        <li>Custom Software Development</li>
+        <li>Web Application Development</li>
+        <li>Mobile App Development</li>
+        <li>DevOps and Cloud Services</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ServicesComponent;
